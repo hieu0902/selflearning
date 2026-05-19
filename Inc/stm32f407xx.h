247d8f6a32d521c0673b71a96c4c767e38191aca
hieu0902 <hieunguyen09022003@gmail.com>
2026-05-20 00:23:34 +0900

    Add base addresses of flash and sram memories

--- Inc/stm32f407xx.h
+++ Inc/stm32f407xx.h
@@ -17,6 +17,10 @@
  * Define base addresses of Flash and SRAM memories
 */
 
+#define FLASH_BASEADDR 0x08000000U
+#define SRAM1_BASEADDR 0x20000000U
+#define SRAM2_BASEADDR 0x2001C000U
+#define ROM_BASEADDR   0x1FFF0000U
 
 /*
  * Revision history 

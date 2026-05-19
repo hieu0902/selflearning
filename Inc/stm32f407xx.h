3033c4c8b5f1825017a2d325b92d56b0857fd0a8
hieu0902 <hieunguyen09022003@gmail.com>
2026-05-20 00:11:49 +0900

    Create template for GPIO driver header file and main header file

---
+++ Inc/stm32f407xx.h
@@ -0,0 +1,24 @@
+/*
+ * stm32f407xx.h
+ *
+ *  Created on: 19-May-2026
+ *      Author: HieuNDM
+ */
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define __vo volatile
+#define __weak __attribute__((weak))
+/*
+ * Define base addresses of Flash and SRAM memories
+*/
+
+
+/*
+ * Revision history 
+    * 19-May-2026: Initial version
+*/
---
+++ Inc/stm32f407xx_gpio_driver.h
@@ -0,0 +1,16 @@
+/*
+ * stm32f407xx_gpio_driver.h
+ *
+ *  Created on: 17-Apr-2024
+ *      Author: HieuNDM
+ */
+#ifndef STM32F407XX_GPIO_DRIVER_H
+#define STM32F407XX_GPIO_DRIVER_H
+
+/*
+
+*/
+/*
+ * Revision history 
+    * 17-Apr-2024: Initial version
+*/
